c95affe21a661383736dea0b6236c92c24dd999a
unknown <pneel361@gmail.com>
2023-08-31 17:28:00 +0530

    javascript operators

---
+++ JavaScript/05.JS_Operators/index.js
@@ -0,0 +1,63 @@
+// Arithmetic Operators
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2; // Addition
+let difference = num1 - num2; // Subtraction
+let product = num1 * num2; // Multiplication
+let quotient = num1 / num2; // Division
+let remainder = num1 % num2; // Modulus
+
+console.log(sum); // Output: 15
+console.log(difference); // Output: 5
+console.log(product); // Output: 50
+console.log(quotient); // Output: 2
+console.log(remainder); // Output: 0
+
+// Assignment Operators
+let value = 10;
+value += 5; // Equivalent to: value = value + 5
+value *= 2; // Equivalent to: value = value * 2
+
+console.log(value); // Output: 30
+
+// Comparison Operators
+let num3 = 15;
+let num4 = 20;
+let isEqual = num3 == num4; // Equality check
+let isNotEqual = num3 != num4; // Inequality check
+let isGreater = num3 > num4; // Greater than
+let isLess = num3 < num4; // Less than
+let isGreaterOrEqual = num3 >= num4; // Greater than or equal
+let isLessOrEqual = num3 <= num4; // Less than or equal
+
+console.log(isEqual); // Output: false
+console.log(isNotEqual); // Output: true
+console.log(isGreater); // Output: false
+console.log(isLess); // Output: true
+console.log(isGreaterOrEqual); // Output: false
+console.log(isLessOrEqual); // Output: true
+
+// Logical Operators
+let isTrue = true;
+let isFalse = false;
+let logicalAnd = isTrue && isFalse; // Logical AND
+let logicalOr = isTrue || isFalse; // Logical OR
+let logicalNot = !isTrue; // Logical NOT
+
+console.log(logicalAnd); // Output: false
+console.log(logicalOr); // Output: true
+console.log(logicalNot); // Output: false
+
+// Increment and Decrement Operators
+let count = 5;
+count++; // Increment by 1
+count--; // Decrement by 1
+
+console.log(count); // Output: 5
+
+// String Concatenation Operator
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName); // Output: John Doe
