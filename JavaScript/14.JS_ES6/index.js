518845ba7f006e95cfa49646f74b836c7f790f3a
unknown <pneel361@gmail.com>
2023-09-01 17:33:43 +0530

    javascript es6

--- JavaScript/14.JS_ES6/index.js
+++ JavaScript/14.JS_ES6/index.js
@@ -3,80 +3,94 @@
 const greet = (name) => {
     return `Hello, ${name}!`;
 };
+
+console.log("Arrow Function:", greet("Rahul"));
   
-  // Template Literals
-  const city = "Mumbai";
-  const greeting = `Welcome to ${city}, the city of dreams!`;
+// Template Literals
+// Template literals, enclosed in backticks, allow for string interpolation and multiline strings, making it easier to create complex string output.
+const city = "Mumbai";
+const greeting = `Welcome to ${city}, the city of dreams!`;
+ 
+console.log("Template Literals:", greeting);
+
+// Destructuring
+// Destructuring provides a concise way to extract values from arrays or objects, improving code readability and reducing the need for multiple variable assignments.
+const [firstCar, secondCar] = ["Maruti", "Hyundai"];
+
+console.log("Destructuring: First Car -", firstCar, "Second Car -", secondCar);
   
-  // Destructuring
-  const [firstCar, secondCar] = ["Maruti", "Hyundai"];
-  
-  // Default Parameters
-  const greetPerson = (name = "Guest") => {
+// Default Parameters
+// Default parameters allow functions to have default values for parameters if no argument or undefined is provided, making functions more flexible and self-explanatory.
+const greetPerson = (name = "Guest") => {
     return `Hello, ${name}!`;
-  };
+};
+
+console.log("Default Parameters:", greetPerson("Priya"));
   
-  // Spread Operator
-  const cities = ["Mumbai", "Delhi", "Bangalore"];
-  const moreCities = ["Chennai", "Kolkata", ...cities];
+// Spread Operator
+// The spread operator (...) allows for the expansion of elements in arrays, objects, and function arguments. It simplifies operations like concatenating arrays.
+const cities = ["Mumbai", "Delhi", "Bangalore"];
+const moreCities = ["Chennai", "Kolkata", ...cities];
+
+console.log("Spread Operator: Cities -", moreCities.join(", "));
   
-  // Classes
-  class Car {
-    constructor(make, model) {
-      this.make = make;
-      this.model = model;
-    }
-  
-    getCarInfo() {
-      return `${this.make} ${this.model}`;
-    }
+// Classes
+// Classes provide a more structured way to define objects and their behavior in JavaScript, following the principles of object-oriented programming (OOP).
+class Car {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
   }
   
-  const myCar = new Car("Maruti", "Swift");
+  getCarInfo() {
+    return `${this.make} ${this.model}`;
+  }
+}
   
-  // Promises
-  const fetchData = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const data = "Data fetched successfully!";
-        resolve(data);
-        // reject("Error fetching data");
-      }, 2000);
-    });
-  };
+const myCar = new Car("Maruti", "Swift");
   
-  // Using Promises
-  fetchData()
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+// Promises
+// Promises are used for handling asynchronous operations in a more organized and readable manner. They offer better error handling and a more structured flow for asynchronous code.
+const fetchData = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = "Data fetched successfully!";
+      resolve(data);
+      // reject("Error fetching data");
+    }, 2000);
+  });
+};
   
-  // Rest Operator
-  const sum = (...numbers) => {
+// Using Promises
+fetchData()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+  
+// Rest Operator
+// The rest operator (...) gathers multiple function arguments into an array, simplifying the handling of variable-length argument lists.
+const sum = (...numbers) => {
     return numbers.reduce((total, number) => total + number, 0);
-  };
+};
+console.log("Classes: My Car -", myCar.getCarInfo()); 
   
-  console.log("Arrow Function:", greet("Rahul"));
-  console.log("Template Literals:", greeting);
-  console.log("Destructuring: First Car -", firstCar, "Second Car -", secondCar);
-  console.log("Default Parameters:", greetPerson("Priya"));
-  console.log("Spread Operator: Cities -", moreCities.join(", "));
-  console.log("Classes: My Car -", myCar.getCarInfo());
+// Set
+// Sets are collections of unique values, allowing you to work with distinct elements. They are useful when you need to ensure uniqueness in a collection of data.
+const uniqueNumbers = new Set();
+uniqueNumbers.add(1);
+uniqueNumbers.add(2);
+uniqueNumbers.add(1); // Duplicates are automatically removed
+
+console.log("Set: Unique Numbers -", Array.from(uniqueNumbers).join(", "));
   
-  // Set
-  const uniqueNumbers = new Set();
-  uniqueNumbers.add(1);
-  uniqueNumbers.add(2);
-  uniqueNumbers.add(1); // Duplicates are automatically removed
+// Map
+// Maps are collections of key-value pairs, providing an efficient way to associate data with keys. They offer better performance than plain objects in certain scenarios.
+const person = new Map();
+person.set("name", "Rahul");
+person.set("age", 30);
   
-  // Map
-  const person = new Map();
-  person.set("name", "Rahul");
-  person.set("age", 30);
-  
-  console.log("Set: Unique Numbers -", Array.from(uniqueNumbers).join(", "));
-  console.log("Map: Name -", person.get("name"), "Age -", person.get("age"));
+console.log("Map: Name -", person.get("name"), "Age -", person.get("age"));
   
