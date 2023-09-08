e5cd12b54e1b2d56137033990e96fc8b4a236db5
unknown <pneel361@gmail.com>
2023-09-08 18:18:33 +0530

    useRef updated

--- React/03.ReactApp/react-app/src/hooks/UseRef/Ref.js
+++ React/03.ReactApp/react-app/src/hooks/UseRef/Ref.js
@@ -4,14 +4,16 @@ function Ref() {
   const inputRef = useRef(null);
 
   const onClick = () => {
+    console.log("inpurRef :", inputRef.current.value);
     inputRef.current.value = "";
+    inputRef.current.focus();
   };
 
   return (
     <>
       <h1>UseRef</h1>
       <input type="text" placeholder="Ex..." ref={inputRef} />
-      <button onClick={onclick}>Change Name</button>
+      <button onClick={onClick}>Change Name</button>
     </>
   );
 }
