7b6ddc6d7d50154faa0bb5ce6a88c65872746136
unknown <pneel361@gmail.com>
2023-09-12 18:51:26 +0530

    redux store  added

---
+++ React/06.redux_toolkit/src/Store/store.js
@@ -0,0 +1,11 @@
+// store.js
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counterSlice";
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+export default store;
