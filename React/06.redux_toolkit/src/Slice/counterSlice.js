e6c1574d55d0d3d912a8a63128e4699c115b3720
unknown <pneel361@gmail.com>
2023-09-12 18:50:50 +0530

    redux counter slice added

---
+++ React/06.redux_toolkit/src/Slice/counterSlice.js
@@ -0,0 +1,20 @@
+// counterSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
