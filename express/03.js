1989a3cf064b9e01f907fc8930348907919df4c9
unknown <pneel361@gmail.com>
2023-09-18 16:25:55 +0530

    middleware

--- express/03.js
+++ express/03.js
@@ -1,3 +1,9 @@
+// Topic 2: Middleware
+
+// Middleware in Express.js
+
+// Middleware functions are an integral part of Express.js. They are functions that have access to the request (req) and response (res) objects in an Express application's request-response cycle. Middleware functions can perform tasks such as authentication, logging, modifying the request or response objects, and more. They are executed in the order they are defined in your Express application.
+
 // Import the Express module
 const express = require('express');
 
@@ -7,7 +13,7 @@ const app = express();
 // Middleware function
 const logger = (req, res, next) => {
   console.log(`Request URL: ${req.url}`);
-  next();
+  next(); // Calls the next middleware in the chain
 };
 
 // Use the middleware
@@ -23,6 +29,10 @@ app.listen(8000, () => {
   console.log('Server is running on port 8000');
 });
 
-// Explanation: Middleware functions are functions that have access to the request and response objects. They can be used to perform tasks like logging, authentication, and more. In this example, we create a simple logger middleware that logs the request URL.
+// In this example:
 
-// Example: When you access http://localhost:8000/, the middleware logs the request URL in the console.
+// We define a custom middleware function logger. It logs the request URL and then calls next(), which passes control to the next middleware or route handler in the chain.
+
+// We use app.use(logger) to apply the middleware to all routes. This means that every incoming request will pass through the logger middleware.
+
+// When you access http://localhost:8000/, the middleware logs the request URL in the console before proceeding to the route handler.
